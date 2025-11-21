fd950d016aa15975523f131e470a3c62d19bb3b8
cekrauseee <henrique@cekrause.eu>
2025-11-21 19:53:28 +0000

    allowing public topics

--- src/domain/communication/topic/repo.ts
+++ src/domain/communication/topic/repo.ts
@@ -1,10 +1,14 @@
 import { database } from '@/domain/infra/db'
-import { and, eq } from 'drizzle-orm'
+import { and, eq, or } from 'drizzle-orm'
 import { topicModel } from './model'
 import type { Topic } from './utils/types'
 
 export const getUserTopics = async (userId: string): Promise<Topic[]> => {
-  const topics = await database.select().from(topicModel).where(eq(topicModel.userId, userId))
+  const topics = await database
+    .select()
+    .from(topicModel)
+    .where(or(eq(topicModel.userId, userId), eq(topicModel.visibility, 'public')))
+
   return topics
 }
 
